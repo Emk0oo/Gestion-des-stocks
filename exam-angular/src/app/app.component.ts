88b3b7735bb2c4bd38795fe72c156818e23d906a
Emir Javor <emir.j2002@gmail.com>
2024-02-20 12:20:15 +0100

    [ADD] Fonction de calcul ok

--- exam-angular/src/app/app.component.ts
+++ exam-angular/src/app/app.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'exam-angular';
+
+  inputNomDuProduit: string= "";
+  inputCalc: string= "";
+  inputResult: string= "";
+  value1: string | null= null;
+  operator: string= "";
+  
+
+  addNumber(number: string){
+    this.value1= number;
+    this.inputCalc+= number;
+
+  }
+
+  execCalc(){
+    console.log('on calcule');
+    this.inputResult=eval(this.inputCalc);
+  }
+
+  clear(){
+    this.inputCalc= "";
+    this.inputResult= "";
+    this.inputNomDuProduit= "";
+  }
 }
+
