968fd7c4e238cdb08439ff9f8767e8b1afe26181
Emir Javor <emir.j2002@gmail.com>
2024-02-20 17:08:41 +0100

    [EDIT] cleaning code, hidding button when md:

--- exam-angular/src/app/app.component.ts
+++ exam-angular/src/app/app.component.ts
@@ -15,53 +15,54 @@ import { item } from '../interface/item.interface';
 export class AppComponent {
   title = 'exam-angular';
 
-  inputNomDuProduit: string= "";
-  inputCalc: string= "";
-  inputResult: string= "";
-  value1: string | null= null;
-  operator: string= "";
-  inputUnite: string= "";
+  inputNomDuProduit: string = '';
+  inputCalc: string = '';
+  inputResult: string = '';
+  value1: string | null = null;
+  operator: string = '';
+  inputUnite: string = '';
   tab: item[] = [];
-  currentId: number |null = null;
+  currentId: number | null = null;
 
-  addNumber(number: string){
-    this.value1= number;
-    this.inputCalc+= number;
-
+  addNumber(number: string) {
+    this.value1 = number;
+    this.inputCalc += number;
   }
 
-  execCalc(){
-    this.inputResult=eval(this.inputCalc);
+  execCalc() {
+    this.inputResult = eval(this.inputCalc);
   }
 
-  clear(){
-    this.inputCalc= "";
-    this.inputResult= "";
-    this.inputNomDuProduit= "";
+  clear() {
+    this.inputCalc = '';
+    this.inputResult = '';
+    this.inputNomDuProduit = '';
   }
 
-  sendToInventory(){
-    console.log('ici')
+  sendToInventory() {
+    console.log('ici');
     let objet = {
       id: Math.random(),
       nomDuProduit: this.inputNomDuProduit,
       quantite: this.inputResult,
       unite: this.inputUnite,
-      calcul: this.inputCalc
-    }
-    if(this.currentId==null){
+      calcul: this.inputCalc,
+    };
+    if (this.currentId == null) {
       this.tab.push(objet);
-    } else{
-      let index : number = this.tab.findIndex((item) => item.id === this.currentId);
+    } else {
+      let index: number = this.tab.findIndex(
+        (item) => item.id === this.currentId
+      );
       this.tab[index] = objet;
       this.currentId = null;
     }
 
-    localStorage.setItem("inventaire", JSON.stringify(this.tab));
+    localStorage.setItem('inventaire', JSON.stringify(this.tab));
   }
 
   ngOnInit() {
-    const storedInventory = localStorage.getItem("inventaire");
+    const storedInventory = localStorage.getItem('inventaire');
     if (storedInventory) {
       this.tab = JSON.parse(storedInventory);
     } else {
@@ -70,51 +71,53 @@ export class AppComponent {
   }
 
   exportCSV() {
-    let date= new Date();
-      var options = { 
+    let date = new Date();
+    var options = {
       fieldSeparator: ',',
       quoteStrings: '"',
       decimalseparator: '.',
-      showLabels: true, 
+      showLabels: true,
       showTitle: true,
-      title: 'Exportation de l\'inventaire',
+      title: "Exportation de l'inventaire",
       useBom: true,
       noDownload: false,
-      headers: ["Nom du Produit", "Quantités", "Unité"],
-      eol: '\n'
+      headers: ['Nom du Produit', 'Quantités', 'Unité'],
+      eol: '\n',
     };
-  
-    new ngxCsv(this.tab, 'exportInventaire'+date, options);
+
+    new ngxCsv(this.tab, 'exportInventaire' + date, options);
   }
 
   print() {
     // Création de la table HTML
     var table = '<table border="1">';
-    table += '<thead><tr><th>Nom</th><th>Quantités</th><th>Unité</th></tr></thead>';
+    table +=
+      '<thead><tr><th>Nom</th><th>Quantités</th><th>Unité</th></tr></thead>';
     table += '<tbody>';
-    for(let i = 0; i < this.tab.length; i++) {
-        table += '<tr>';
-        table += '<td>' + this.tab[i].nomDuProduit + '</td>';
-        table += '<td>' + this.tab[i].quantite + '</td>';
-        table += '<td>' + this.tab[i].unite + '</td>';
-        table += '</tr>';
-    };
+    for (let i = 0; i < this.tab.length; i++) {
+      table += '<tr>';
+      table += '<td>' + this.tab[i].nomDuProduit + '</td>';
+      table += '<td>' + this.tab[i].quantite + '</td>';
+      table += '<td>' + this.tab[i].unite + '</td>';
+      table += '</tr>';
+    }
     table += '</tbody>';
     table += '</table>';
 
     document.body.innerHTML = table;
 
     window.print();
+  }
+
+  update(item: item) {
+    this.inputNomDuProduit = item.nomDuProduit;
+    this.inputCalc = item.calcul;
+    this.inputResult = item.quantite;
+    this.inputUnite = item.unite;
+    this.currentId = item.id;
+  }
+
+  import(){
+    alert('Fonctionnalité non implémentée');
+  }
 }
-
-update(item: item) {
-  this.inputNomDuProduit = item.nomDuProduit;
-  this.inputCalc = item.calcul;
-  this.inputResult = item.quantite;
-  this.inputUnite = item.unite;
-  this.currentId = item.id;
-
-
-}
-}
-
